834e880e15c49a4b260f368aa8027ece7db55005
박재황 <woghkd0719@gmail.com>
2023-01-04 17:21:46 +0900

    안내 문구 변경

--- hello.c
+++ hello.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[], char* envp[]) {
-    printf("hello World!\n");
-    printf("hello Git!\n");
-    printf("hello Git!\n");
+    printf("hello Study!\n");
 
     return 0;
 }
